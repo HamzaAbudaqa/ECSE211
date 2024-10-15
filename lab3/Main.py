9771be460df6816fd2f1bdbfe1d4a299fb64e122
Marianne Romero <marianne.romero@mail.mcgill.ca>
2024-10-15 12:28:58 -0400

    adding threading (draft)

---
+++ lab3/Main.py
@@ -0,0 +1,34 @@
+import threading, logging, subprocess
+from utils.brick import TouchSensor, wait_ready_sensors
+
+TOUCH_SENSOR = TouchSensor(1)
+
+def start_rythm():
+    logging.info("Starting the Rythm.py")
+    subprocess.run(["python3", "Rythm.py"])
+
+def emergency_stop():
+    logging.info("Emergency stop triggered")
+    exit()
+
+
+def create_threads():
+    threads = []
+    
+    # Rythm thread
+    rythm = threading.Thread(target=start_rythm, args=("Thread-Rythm",))
+    rythm.daemon = True
+    threads.append(rythm)
+    rythm.start()
+
+if __name__ == "__main__":
+    try:
+        while True:
+            if TOUCH_SENSOR.is_pressed():
+                emergency_stop()
+    except BaseException:  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
+        exit()
+
+
+
+
