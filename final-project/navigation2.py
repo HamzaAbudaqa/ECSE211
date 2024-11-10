208b888fe94434b1a091de36244bf343ca5fa1f1
Marianne Romero <marianne.romero@mail.mcgill.ca>
2024-11-10 11:14:11 -0500

    improving readbaility of navigation2.py code

--- final-project/navigation2.py
+++ final-project/navigation2.py
@@ -7,8 +7,8 @@ US_POLL_DELAY = 0.1
 RW = 0.028 # wheel radius
 RB = 0.09  # axle length
 
-DIST_TO_DEG = 180/(3.1416*RW)   # scale factor for distance
-ORIENT_TO_DEG = RB/RW           # scale factor for rotation
+DIST_TO_DEG = 180/(3.1416*RW)  # scale factor for distance
+ORIENT_TO_DEG = RB/RW          # scale factor for rotation
 
 LEFT_MOTOR = Motor('A')
 RIGHT_MOTOR = Motor('B')
@@ -24,6 +24,10 @@ TRN_SPEED = 380
 # bang bang controller constants
 DEADBAND = 10 # degrees
 DELTA_SPEED = 50 # dps
+
+# put value small enough so that if it's following the wall
+# the distance measured from the side won't have an impact
+MIN_DIST_FROM_WALL = 3 # cm
 
 # sensors
 GYRO = EV3GyroSensor(port=1, mode="abs")
@@ -53,10 +57,17 @@ def init_motors():
 
 
 def move_fwd_until_wall(angle):
+    """
+    Makes the robot go in a staright line at the given angle (absolute angle
+    rotated since start) by implementing the bang bang controller
+
+    The robot stops once it finds itself at a distance smaller than 3cm from
+    a wall
+    """
     try:
         LEFT_MOTOR.set_limits(POWER_LIMIT, FWD_SPEED)
         RIGHT_MOTOR.set_limits(POWER_LIMIT, FWD_SPEED)
-        while (US_SENSOR.get_value() > 3): # put value small enough so that if it's following the wall it doesn't matter
+        while (US_SENSOR.get_value() > MIN_DIST_FROM_WALL):
             # bang bang controller
             error = GYRO.get_abs_measure() - angle
             if (abs(error) <= DEADBAND): # no correction
@@ -74,19 +85,17 @@ def move_fwd_until_wall(angle):
         print(error)
 
         
-def rotate(angle):
+def rotate(angle, speed):
     """
     In-place rotation for the given angle
-
-    angle > 0: rotate right
-
-    angle < 0: rotate left
+    - angle > 0: rotate right
+    - angle < 0: rotate left
     """
     try:
-        LEFT_MOTOR.set_dps(TRN_SPEED)
-        RIGHT_MOTOR.set_dps(TRN_SPEED)
-        LEFT_MOTOR.set_limits(POWER_LIMIT, TRN_SPEED)
-        RIGHT_MOTOR.set_limits(POWER_LIMIT, TRN_SPEED)
+        LEFT_MOTOR.set_dps(speed)
+        RIGHT_MOTOR.set_dps(speed)
+        LEFT_MOTOR.set_limits(POWER_LIMIT, speed)
+        RIGHT_MOTOR.set_limits(POWER_LIMIT, speed)
         LEFT_MOTOR.set_position_relative(int(angle * ORIENT_TO_DEG)) 
         RIGHT_MOTOR.set_position_relative(-int(angle * ORIENT_TO_DEG))
 
@@ -98,11 +107,10 @@ def rotate(angle):
 
 def rotate_at_wall(dir: str):
     """
-    Rotates the robot in the given direction and positions itself in the next row to sweep
-
-    dir = right : right rotate (go from -180 to 0 deg on gyro)
-
-    dir = -1 : left rotate (go from 0 to -180 deg on gyro)
+    Rotates the robot in the given direction and positions itself in 
+    the next row to sweep
+    - dir = "right" : right rotate (go from -180 to 0 deg on gyro)
+    - dir = "left" : left rotate (go from 0 to -180 deg on gyro)
     """
     try:
         # go to -90 deg on gyro
@@ -135,13 +143,24 @@ def do_s_shape():
     time.sleep(0.15)
     move_fwd_until_wall(0)
     time.sleep(0.15)
-    rotate_at_wall(-1) # want to go to -180 angle
+    rotate_at_wall("left") # going to angle -180 on gyro
     # going in opposite dir
     time.sleep(0.15)
-    move_fwd_until_wall(180)
+    move_fwd_until_wall(-180)
     time.sleep(0.15)
-    rotate_at_wall(+1) # want to go to 0 angle
+    rotate_at_wall("right") # going to angle 0 on gyro
 
+
+def get_back_to_start():
+    """
+    Gets the robot back to the start area given that the start area
+    is in direction -270 deg (90 deg) abs angle on gyro
+    """
+    # initially, robot is at wall
+    time.sleep(0.15)
+    rotate(-270 - GYRO.get_abs_measure(), TRN_SPEED) # turn in direction of start area
+    move_fwd_until_wall(90)
+    
 
 def stop():
     time.sleep(0.15)
@@ -149,19 +168,23 @@ def stop():
     LEFT_MOTOR.set_power(0)
     time.sleep(0.15)
 
-try: 
-    print("Navigation program started")
-    init_motors()
-    for i in range(NB_S):
-        do_s_shape()
-    # get back to start position
-    rotate(90) # or -270
-    move_fwd_until_wall()
-    print("Finished sweep of the map")
-    print("Navigation program ended")
-except KeyboardInterrupt:
-    print("Navigation program terminated")
-finally:
-    stop()
-    reset_brick()
-    print("Navigation stopped")
+
+def navigation_program():
+    try: 
+        print("Navigation program started")
+        init_motors()
+        for i in range(NB_S):
+            do_s_shape()
+        # get back to start position
+        get_back_to_start()
+        print("Finished sweep of the map")
+        print("Navigation program ended")
+    except KeyboardInterrupt:
+        print("Navigation program terminated")
+    finally:
+        stop()
+        reset_brick()
+
+
+if __name__ == "__main__":
+    navigation_program()
