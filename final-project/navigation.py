287609f26eafa8bf8c284bf84746b8594f860c09
Marianne Romero <marianne.romero@mail.mcgill.ca>
2024-11-11 14:47:24 -0500

    navigation does s motions correctly

--- final-project/navigation.py
+++ final-project/navigation.py
@@ -2,24 +2,27 @@ from utils.brick import Motor, reset_brick
 import time
 import math
 
-MOTOR_POLL_DELAY = 0.05
+MOTOR_POLL_DELAY = 0.1
+MOTOR_SLEEP = 0.15
 
-RW = 0.028 # wheel radius
-RB = 0.09  # axle length
+RW = 0.022 # wheel radius
+RB = 0.11  # axle length
 
-DIST_TO_DEG = 180/(3.1416*RW)   # scale factor for distance
+DIST_TO_DEG = 180/(math.pi*RW)   # scale factor for distance
 ORIENT_TO_DEG = RB/RW           # scale factor for rotation
 
 LEFT_MOTOR = Motor('A')
 RIGHT_MOTOR = Motor('B')
-POWER_LIMIT = 70
+POWER_LIMIT = 600
 SPEED_LIMIT = 720
 
-ROBOT_LEN = 15 # cm
-MAP_SIZE = 120 # cm
-NB_S = (int) (MAP_SIZE/ROBOT_LEN)/2
-FWD_SPEED = 100
-TRN_SPEED = 180
+# sizes had to be changed otherwise the turns weren't made properly
+# so i adjusted the sized of the wheels to be smaller
+ROBOT_LEN = 0.15
+MAP_SIZE = 1.00
+NB_S = int((MAP_SIZE/ROBOT_LEN)/2)
+FWD_SPEED = 300
+TRN_SPEED = 300
 
 
 def wait_for_motor(motor: Motor):
@@ -57,8 +60,8 @@ def move_dist_fwd(distance, speed):
 
         
 def rotate(angle, speed):
-    # angle > 0: rotate right
-    # angle < 0: rotate left
+    # angle < 0: rotate right
+    # angle > 0: rotate left
     try:
         LEFT_MOTOR.set_dps(speed)
         RIGHT_MOTOR.set_dps(speed)
@@ -66,6 +69,7 @@ def rotate(angle, speed):
         RIGHT_MOTOR.set_limits(POWER_LIMIT, speed)
         LEFT_MOTOR.set_position_relative(int(angle * ORIENT_TO_DEG)) 
         RIGHT_MOTOR.set_position_relative(-int(angle * ORIENT_TO_DEG))
+        wait_for_motor(RIGHT_MOTOR)
     except IOError as error:
         print(error)
 
@@ -74,9 +78,9 @@ def right_rotate_at_wall():
     # right rotates at wall and positions itself in
     # the next row to sweep
     try:
-        rotate(90, TRN_SPEED)
+        rotate(-90, TRN_SPEED)
         move_dist_fwd(ROBOT_LEN, FWD_SPEED)
-        rotate(90, TRN_SPEED)
+        rotate(-90, TRN_SPEED)
     except IOError as error:
         print(error)
 
@@ -85,21 +89,17 @@ def left_rotate_at_wall():
     # left rotates at wall and positions itself in
     # the next row to sweep
     try:
-        rotate(-90, TRN_SPEED)
+        rotate(+90, TRN_SPEED)
         move_dist_fwd(ROBOT_LEN, FWD_SPEED)
-        rotate(-90, TRN_SPEED)
+        rotate(+90, TRN_SPEED)
     except IOError as error:
         print(error)
 
 
 def do_s_shape():
-    time.sleep(0.15)
     move_dist_fwd(MAP_SIZE, FWD_SPEED)
-    time.sleep(0.15)
     left_rotate_at_wall()
-    time.sleep(0.15)
     move_dist_fwd(MAP_SIZE, FWD_SPEED)
-    time.sleep(0.15)
     right_rotate_at_wall()
 
 
@@ -113,8 +113,12 @@ def stop():
 try: 
     print("Navigation started")
     init_motors()
-    for i in range(NB_S):
-        do_s_shape()
+    for i in range(0,NB_S):
+        time.sleep(0.15)
+        move_dist_fwd(MAP_SIZE,FWD_SPEED)
+        left_rotate_at_wall()
+        move_dist_fwd(MAP_SIZE,FWD_SPEED)
+        right_rotate_at_wall()
     # get back to start position
     right_rotate_at_wall()
     move_dist_fwd(MAP_SIZE, FWD_SPEED)
--- final-project/navigation2.py
+++ final-project/navigation2.py
@@ -1,37 +1,38 @@
 from utils.brick import EV3GyroSensor, EV3UltrasonicSensor, Motor, reset_brick, wait_ready_sensors
 import time, math
 
-MOTOR_POLL_DELAY = 0.15
+MOTOR_POLL_DELAY = 0.2
 US_POLL_DELAY = 0.1
 
-RW = 0.028 # wheel radius
-RB = 0.09  # axle length
+RW = 0.022 # wheel radius
+RB = 0.04  # axle length
 
 DIST_TO_DEG = 180/(3.1416*RW)  # scale factor for distance
 ORIENT_TO_DEG = RB/RW          # scale factor for rotation
 
-LEFT_MOTOR = Motor('A')
-RIGHT_MOTOR = Motor('B')
-POWER_LIMIT = 500
+RIGHT_MOTOR = Motor('A')
+LEFT_MOTOR = Motor('B')
+POWER_LIMIT = 400
 SPEED_LIMIT = 720
 
-ROBOT_LEN = 15 # cm
+ROBOT_LEN = 0.15 # cm
 MAP_SIZE = 120 # cm
-NB_S = (int) (MAP_SIZE/ROBOT_LEN)/2 # number of back and forth s motions to cover the entire board
+NB_S = int((MAP_SIZE/ROBOT_LEN)/2) # number of back and forth s motions to cover the entire board
 FWD_SPEED = 300
-TRN_SPEED = 380
+TRN_SPEED = 320
 
 # bang bang controller constants
 DEADBAND = 10 # degrees
-DELTA_SPEED = 50 # dps
+DELTA_SPEED = 40 # dps
 
 # put value small enough so that if it's following the wall
 # the distance measured from the side won't have an impact
-MIN_DIST_FROM_WALL = 3 # cm
+MIN_DIST_FROM_WALL = 10 # cm
 
 # sensors
 GYRO = EV3GyroSensor(port=1, mode="abs")
 US_SENSOR = EV3UltrasonicSensor(2)
+print("waiting for sensors")
 wait_ready_sensors()
 
 
@@ -67,16 +68,25 @@ def move_fwd_until_wall(angle):
     try:
         LEFT_MOTOR.set_limits(POWER_LIMIT, FWD_SPEED)
         RIGHT_MOTOR.set_limits(POWER_LIMIT, FWD_SPEED)
+        i = 0
         while (US_SENSOR.get_value() > MIN_DIST_FROM_WALL):
+            if (i%5 != 0):# increase delay for bang bang controller
+                time.sleep(0.2)
+                continue
             # bang bang controller
-            error = GYRO.get_abs_measure() - angle
+            gyro_angle = GYRO.get_abs_measure()
+            #print("current angle is: " + str(gyro_angle))
+                  
+            error = gyro_angle - angle
             if (abs(error) <= DEADBAND): # no correction
                 LEFT_MOTOR.set_dps(FWD_SPEED)
                 RIGHT_MOTOR.set_dps(FWD_SPEED)
             elif (error > 0): # angle too big
+                #print("increasing right motor speed")
                 LEFT_MOTOR.set_dps(FWD_SPEED)
                 RIGHT_MOTOR.set_dps(FWD_SPEED + DELTA_SPEED)
             else: # angle too small
+                #print("increasing left motor speed")
                 LEFT_MOTOR.set_dps(FWD_SPEED + DELTA_SPEED)
                 RIGHT_MOTOR.set_dps(FWD_SPEED)
             time.sleep(US_POLL_DELAY)
@@ -114,9 +124,11 @@ def rotate_at_wall(dir: str):
     """
     try:
         # go to -90 deg on gyro
-        if (dir == "right"): 
-            rotate(-90 + GYRO.get_abs_measure(), TRN_SPEED)
+        if (dir == "right"):
+            print("rotating right")
+            rotate(-90 - GYRO.get_abs_measure(), TRN_SPEED)
         else:
+            print("rotating left 1")
             rotate(-90 - GYRO.get_abs_measure() , TRN_SPEED)
 
         # go straight for the length of the robot
@@ -133,10 +145,22 @@ def rotate_at_wall(dir: str):
             rotate( - GYRO.get_abs_measure(), TRN_SPEED)
         else:
             # go to -180 deg on gyro
+            print("rotating left 2 ")
             rotate(-180 - GYRO.get_abs_measure() , TRN_SPEED)
     except IOError as error:
         print(error)
 
+def move_fwd(distance):
+    try:
+        LEFT_MOTOR.set_dps(FWD_SPEED)
+        RIGHT_MOTOR.set_dps(FWD_SPEED)
+        LEFT_MOTOR.set_limits(POWER_LIMIT, FWD_SPEED)
+        RIGHT_MOTOR.set_limits(POWER_LIMIT, FWD_SPEED)
+        LEFT_MOTOR.set_position_relative(int(ROBOT_LEN*DIST_TO_DEG))
+        RIGHT_MOTOR.set_position_relative(int(ROBOT_LEN*DIST_TO_DEG))
+        wait_for_motor(RIGHT_MOTOR)
+    except IOError as error:
+        print(error)
 
 def do_s_shape():
     # going in initial dir
@@ -145,6 +169,8 @@ def do_s_shape():
     time.sleep(0.15)
     rotate_at_wall("left") # going to angle -180 on gyro
     # going in opposite dir
+    time.sleep(0.15)
+    move_fwd(ROBOT_LEN)
     time.sleep(0.15)
     move_fwd_until_wall(-180)
     time.sleep(0.15)
@@ -163,6 +189,7 @@ def get_back_to_start():
     
 
 def stop():
+    "Stop left and right motors"
     time.sleep(0.15)
     RIGHT_MOTOR.set_power(0)
     LEFT_MOTOR.set_power(0)
@@ -170,6 +197,7 @@ def stop():
 
 
 def navigation_program():
+    "Make an entire sweep of the board and go back to start position"
     try: 
         print("Navigation program started")
         init_motors()
