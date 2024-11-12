68e751268e14ca090b8de9c99893fc63e982f3a0
ReinaEl-Hoz <rehz2004@gmail.com>
2024-11-12 11:45:43 -0500

    dumper and grabber updated code

--- final-project/grabber.py
+++ final-project/grabber.py
@@ -1,21 +1,26 @@
+
 from utils.brick import Motor, configure_ports, reset_brick
 from time import sleep
 
 
 CLAW_MOTOR = Motor('A')
 LIFT_MOTOR = Motor('B')
-GRAB_POSITION = 0
-#RELEASE_POSITION = -250
-LIFT_UP_POSITION = 225
+GRAB_POSITION = 300
+HIT_POSITION = 250
+LIFT_UP_POSITION = -240
+HITTING_POSITION = -210
+LIFT_HALF_UP_POSITION = -50
 LIFT_DOWN_POSITION = 0
 DUMP_PUSH_POSITION = 45
 
 POWER_LIMIT = 50
 SPEED_LIMIT = 250
+HIGH_POWER_LIMIT = 100
+HIGH_SPEED_LIMIT = 400
 LIFT_POWER_LIMIT = 70
 LIFT_SPEED_LIMIT = 200
-
 DUMP_WAIT_TIME = 3
+
 def grab_and_release():
     ''' Function to grab a block lift it & release it into storage unit '''
     
@@ -25,14 +30,13 @@ def grab_and_release():
         initial_claw_position = CLAW_MOTOR.get_position()
         initial_lift_position = LIFT_MOTOR.get_position()
         print(initial_lift_position, initial_claw_position)
+
         CLAW_MOTOR.set_limits(POWER_LIMIT, SPEED_LIMIT)
         LIFT_MOTOR.set_limits(LIFT_POWER_LIMIT, LIFT_SPEED_LIMIT)
 
 
         print("Grabbing the block now.")
-
-        CLAW_MOTOR.set_position_relative(GRAB_POSITION)
-        CLAW_MOTOR.set_power(POWER_LIMIT)
+        CLAW_MOTOR.set_position(initial_claw_position)
         sleep(1)
         
         print ("Lifting the block to storage unit")
@@ -40,39 +44,56 @@ def grab_and_release():
         sleep(1)  
 
         print ("Releasing the block")
-        CLAW_MOTOR.set_position(initial_claw_position)
+        CLAW_MOTOR.set_position(20)
         sleep(1)
 
         print ("Returning claw to initial position")
         LIFT_MOTOR.set_position(initial_lift_position)
-        sleep(1)
+        sleep(4)
+        
+        print ("Returning claw to normal mode")
+        CLAW_MOTOR.set_position(initial_claw_position)
+
     except IOError as error:
         print(f"Error: {error}")
 
 def dump_storage():
     ''' function to dump blocks inside storage unit into the trash'''
     try:
+
         LIFT_MOTOR.reset_encoder()
         CLAW_MOTOR.reset_encoder()
         initial_claw_position = CLAW_MOTOR.get_position()
         initial_lift_position = LIFT_MOTOR.get_position()
-        print(initial_lift_position, initial_claw_position)
+
         CLAW_MOTOR.set_limits(POWER_LIMIT, SPEED_LIMIT)
         LIFT_MOTOR.set_limits(LIFT_POWER_LIMIT, LIFT_SPEED_LIMIT)
 
         print ("Starting the dumping operation")
 
-        LIFT_MOTOR.set_position(LIFT_UP_POSITION)
-        sleep(2)
+        print("Lifting the arm slightly")
+        LIFT_MOTOR.set_position(LIFT_HALF_UP_POSITION)
+        sleep(1)
 
+        print("Rotating claw to hitting position")
+        CLAW_MOTOR.set_position_relative(HIT_POSITION)
+        sleep(1)
 
-        print("Retracting the arm now")
-        LIFT_MOTOR.set_position(LIFT_DOWN_POSITION)
-        sleep(2)
+        print("Hitting storage unit with momentum")
+        LIFT_MOTOR.set_limits(HIGH_POWER_LIMIT, HIGH_SPEED_LIMIT)
+        LIFT_MOTOR.set_position(HITTING_POSITION)
+        sleep(1)
 
+        print("Resetting to initial positions")
+        CLAW_MOTOR.set_position(initial_claw_position)
+        LIFT_MOTOR.set_position(initial_lift_position)
+        sleep(1)
+        
         print ("Mission accomplished")
+
     except IOError as error:
         print (f"Error during dumping operation: {error}")
+
 
 
 dump_storage()
